351e9247824b5fe7821994abf97b9cfae70ef941
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2025-06-01 18:22:37 +0200

    Enable current eslint-plugin-vue

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import eslintPluginVue from 'eslint-plugin-vue';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from "eslint/config";
 import js from '@eslint/js'
+import globals from 'globals';
 
-export default [
+export default defineConfig([
   {
     ignores: [
       '**/dist/*',
@@ -14,6 +16,10 @@ export default [
   {
     languageOptions: {
       ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
-];
+]);
